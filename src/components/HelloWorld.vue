fd59e31126e403710e4da7a8eb4c862540e5eeb3
aeyfaith <sameanmart@gmail.com>
2023-09-08 02:25:05 +0700

    bg-home

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,33 +4,34 @@
 // const count = ref(0)
 </script>
 
-<template>
+<template >
   <!-- <h1>{{ msg }}</h1> -->
 
   <!-- <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>  
   </div> -->
-  <div class="head-login col-12 container">
-    <div class="head-text col-5">
+  <div class="head-login col-12 container bg-white">
+    <div class="head-text col-4 m-auto">
       <img src="src/assets/icon01.png" class="head-icon">
       <div>Home</div>
       <div>Service</div>
       <div>About Us</div>
       <div>Blog</div>
     </div>
-    <div class="search-container col-4">
+    <div class="search-container col-4" >
       <form action="" class="d-flex form-search">
         <input
           type="text"
           placeholder="Search.."
           name="search"
           class="input-search"
+          
         />
         <button type="submit"><i class="fa fa-search"></i></button>
       </form>
     </div>
-    <div class="col-2 t-call ">
-     <div> <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16" color="#264971"> <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+    <div class="col-2 t-call m-auto">
+     <div> <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16" color="#264971"> <path fill-rule="evenodd"  d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
       </svg></div>
       <div style="margin-left:1rem;">
       <div>Call me</div>
@@ -38,9 +39,23 @@
     </div>
     </div>
   </div>
+
+  
+  <div>
+    ccccc
+  </div><div>
+    ccccc
+  </div><div>
+    ccccc
+  </div><div>
+    ccccc
+  </div>
+
+
 </template>
 
 <style scoped>
+
 .read-the-docs {
   color: #888;
 }
@@ -58,6 +73,7 @@
   border: none;
   border-radius: 1rem;
   cursor: pointer;
+  color:#fff;
 }
 .input-search {
   width: -webkit-fill-available;
